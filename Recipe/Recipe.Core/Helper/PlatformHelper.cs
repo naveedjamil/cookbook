03b3937a2e91659965853f064439c9080e0e2c9e
10PEARLS\tahir.hassan <tahir.hassan@tenpearls.com>
2017-11-15 11:57:53 +0500

    - Fixing if else issue - Remove extra function

--- Recipe/Recipe.Core/Helper/PlatformHelper.cs
+++ Recipe/Recipe.Core/Helper/PlatformHelper.cs
@@ -12,73 +12,36 @@ namespace Recipe.Core.Helper
             {
                 return "Android";
             }
-
-            if (ua.Contains("iPad"))
+            else if (ua.Contains("iPad"))
             {
                 return "iPad";
             }
-
-            if (ua.Contains("iPhone"))
+            else if (ua.Contains("iPhone"))
             {
                 return "iPhone";
             }
-
-            if (ua.Contains("Linux") && ua.Contains("KFAPWI"))
+            else if (ua.Contains("Linux") && ua.Contains("KFAPWI"))
             {
                 return "Linux";
             }
-
-            if (ua.Contains("RIM Tablet") || (ua.Contains("BB") && ua.Contains("Mobile")))
+            else if (ua.Contains("RIM Tablet") || (ua.Contains("BB")))
             {
                 return "BlackBerry";
             }
-
-            if (ua.Contains("Windows Phone"))
+            else if (ua.Contains("Windows Phone"))
             {
                 return "WindowsPhone";
             }
-
-            if (ua.Contains("Mac OS"))
+            else if (ua.Contains("Mac OS"))
             {
                 return "MacOS";
             }
-
-            if (ua.Contains("Windows"))
+            else if (ua.Contains("Windows"))
             {
                 return "Windows";
             }
+
             return string.Empty;
-        }
-
-        private static string GetMobileVersion(string userAgent, string device)
-        {
-            var temp = userAgent.Substring(userAgent.IndexOf(device) + device.Length).TrimStart();
-            var version = string.Empty;
-
-            foreach (var character in temp)
-            {
-                var validCharacter = false;
-                int test = 0;
-
-                if (int.TryParse(character.ToString(), out test))
-                {
-                    version += character;
-                    validCharacter = true;
-                }
-
-                if (character == '.' || character == '_')
-                {
-                    version += '.';
-                    validCharacter = true;
-                }
-
-                if (validCharacter == false)
-                {
-                    break;
-                }
-            }
-
-            return version;
         }
     }
 }
